4b6378687a05c634425405b03f95d06823cb177d
unknown <you@example.com>
2018-01-28 16:58:31 -0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function(grunt) {
+    "use strict";
+
+		//here we will configure our plugging
+		
+		 grunt.initConfig({
+			
+			concat: {
+
+				js: {
+					src: ['js/*.js'],
+					dest: 'build/scripts.js'
+				},
+				css: {
+					src: ['css/*.css'],
+					dest: 'build/style.css'
+				}
+			},
+			
+			// after concat minify main file
+			
+			cssmin: {
+			  target: {
+				files: [{
+				  expand: true,
+				  src: ['*.css', '!*.min.css'],
+				  dest: 'build/style',
+				  ext: '.min.css'
+				}]
+			  }
+			}
+							 
+		 });
+		 
+		
+		// after config we need to load it 
+		grunt.loadNpmTasks('grunt-contrib-cssmin');
+		grunt.loadNpmTasks('grunt-contrib-concat');
+		
+		// register your task for future use
+		
+		grunt.registerTask('concat-js', ['concat:js']);
+		grunt.registerTask('concat-css', ['concat:css']);
+		
+		
+		
+		
+
+	}
